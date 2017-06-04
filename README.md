74a829d8312798d0d979815d742c86eb347e8db3
xaicron <xaicron@gmail.com>
2017-06-05 00:39:13 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WWW::FCM::HTTP - It's new $module
+
+# SYNOPSIS
+
+    use WWW::FCM::HTTP;
+
+# DESCRIPTION
+
+WWW::FCM::HTTP is ...
+
+# LICENSE
+
+Copyright (C) xaicron.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+xaicron <xaicron@gmail.com>
---
+++ lib/WWW/FCM/HTTP.pm
@@ -0,0 +1,39 @@
+package WWW::FCM::HTTP;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::FCM::HTTP - It's new $module
+
+=head1 SYNOPSIS
+
+    use WWW::FCM::HTTP;
+
+=head1 DESCRIPTION
+
+WWW::FCM::HTTP is ...
+
+=head1 LICENSE
+
+Copyright (C) xaicron.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron@gmail.comE<gt>
+
+=cut
+
